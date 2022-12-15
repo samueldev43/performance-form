11830995ed324675f54997d78aff7eb6e1232cdc
Samuel <71525496+Sam554-dev@users.noreply.github.com>
2022-12-15 08:03:47 -0300

    Update style.ts

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -7,12 +7,10 @@ max-width: 300px;
 padding: 0.5rem;
 background: transparent;
 border: 0;
-outline: 0;
 border-bottom: 1px solid #f0f0f5;
 color: #ffffff;
 
 &:focus {
     border-color: salmon;
 }
-
 `
